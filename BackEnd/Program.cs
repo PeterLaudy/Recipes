b63fab6c2a2f1d4d72939f66aa61a828cc4ca65b
Peter Laudy <github@zestien3.nl>
2025-01-13 12:55:02 +0000

    Program will take command line parameter to extra configuration file.
    Fixes #11

--- BackEnd/Program.cs
+++ BackEnd/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-
 
 namespace Recepten
 {
@@ -9,14 +9,21 @@ namespace Recepten
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args)
-            .UseUrls(["http://0.0.0.0:8080/"])
-            .Build()
-            .Run();
-        }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .ConfigureAppConfiguration((hostingContext, configBuilder) =>
+                    {
+                        // When running this in a docker image, we want to be able
+                        // to change the configuration without rebuilding the image.
+                        // The file to use must be the first parameter on the command line.
+                        if (args.Length >= 1)
+                        {
+                            configBuilder.AddJsonFile(path: args[0], optional: true, reloadOnChange: false);
+                        }
+                    })
+                .UseStartup<Startup>()
+                .UseUrls(["http://0.0.0.0:8080/"])
+                .Build()
+                .Run();
+        }
     }
 }
